70f4f5f0782fdfb3bbf0176d85356777a7e4dbad
Himalaya vats <contact@himalayavats.com>
2021-09-28 03:12:16 +0530

    new

--- src/utils/prepareCarSearchResults.js
+++ src/utils/prepareCarSearchResults.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
 const prepareCarSearchResults = (customerId, bankId, profitAmount) => {
-    const endpoint = '/carSearch/cars/search';
-
-    // const url = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_CLOUD_FUNCTIONS_URL_PROD + endpoint : process.env.REACT_APP_CLOUD_FUNCTIONS_URL_DEV + endpoint;
-
-    const url = "https://us-central1-cars-development-1f062.cloudfunctions.net/carSearch/cars/search";
+    const url = process.env.REACT_APP_CLOUD_FUNCTIONS_URL + '/carSearch/cars/search';
 
     axios.post(url, {
         customerId: customerId,
--- src/utils/upload.js
+++ src/utils/upload.js
@@ -1,9 +1,7 @@
 import axios from "axios"
 
 const upload = (options) => {
-    // const url = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_CLOUD_FUNCTIONS_URL_PROD + options.endpoint : process.env.REACT_APP_CLOUD_FUNCTIONS_URL_DEV + options.endpoint;
-
-    const url = "https://us-central1-cars-development-1f062.cloudfunctions.net" + options.endpoint
+    const url = process.env.REACT_APP_CLOUD_FUNCTIONS_URL + options.endpoint;
 
     const fd = new FormData()
     fd.append("file" , options.file);
@@ -15,6 +13,6 @@ const upload = (options) => {
     }).catch((err) => {
         console.log(err);
     })
-}
+  }
 
   export default upload;
